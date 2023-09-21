aa580a30fbfdc53f90eba94600a40d41df2a2284
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2023-09-21 22:27:45 +0900

    Update and rename File_001_Java.java to File_001_HellowWorld.java

--- File_001_HellowWorld.java
+++ File_001_HellowWorld.java
@@ -1,4 +1,4 @@
-public class File_001Java{
+public class File_001_HellowWorld{
     public static void main(String aegs[]){
     System.out.println("Hellow World");
     System.out.println("My First Java(Desktop PC)");
