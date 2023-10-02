a8728cd2b3b35347f69245e7b8fa7341c56ebb39
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2023-10-02 12:40:48 +0900

    Update File_022_addition.java

--- File_022_addition.java
+++ File_022_addition.java
@@ -1,10 +1,10 @@
-public class File_022_addition{
-    public static void main(String args[]){
-        int n=1,sum=0;
-        while(n<=1000000){
-            System.out.print("n="+n);
-            sum+=n;
-            System.out.println("\t累計値="+sum);
+public class File_022_addition {
+    public static void main(String args[]) {
+        int n = 1, sum = 0;
+        while (n <= 1000000) {
+            System.out.print("n=" + n);
+            sum += n;
+            System.out.println("\t累計値=" + sum);
             n++;
         }
         System.out.println("ループ終了");
